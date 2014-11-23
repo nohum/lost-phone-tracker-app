95ec409eb5738c9f4cfd07cc83ec25d92d434e29
nohum <eiskalterengel@truthfactory.tk>
2014-11-23 20:29:13 +0100

    added classes and basic permissions

---
+++ src/main/java/at/fhj/ase13/mobcomp2/phonetracker/LogAlarmReceiver.java
@@ -0,0 +1,17 @@
+package at.fhj.ase13.mobcomp2.phonetracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class LogAlarmReceiver extends BroadcastReceiver {
+    public LogAlarmReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
