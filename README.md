f00b6763979fc81ee060f90e9d08e2ff1ee002fd
nohum <eiskalterengel@truthfactory.tk>
2014-12-07 17:47:23 +0100

    activity design

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ lost-phone-tracker-app
 ========================
 
 Phone tracker for Android - if your phone has been stolen
+
+Resources
+---------
+
+Icon: https://www.iconfinder.com/icons/283048/google_maps_locate_location_map_marker_navigation_pin_pointer_position_icon
--- src/main/java/at/fhj/ase13/mobcomp2/phonetracker/MainActivity.java
+++ src/main/java/at/fhj/ase13/mobcomp2/phonetracker/MainActivity.java
@@ -2,8 +2,6 @@ package at.fhj.ase13.mobcomp2.phonetracker;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
@@ -14,26 +12,4 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main_activity);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
